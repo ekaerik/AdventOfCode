a7d2932d93bde529af09ef9085a9ab26600dd593
Unknown <eka.sundberg@gmail.com>
2017-05-22 22:40:28 +0200

    Day3

---
+++ AdventOfCOde/Day3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCOde
+{
+	public class Day3
+	{
+		public int NumberOfTriangles(IEnumerable<int[]> examples)
+		{
+			var validTriangles = 0;
+
+			foreach(var example in examples)
+			{
+				if (example[1] + example[2] > example[0] &&
+					example[0] + example[2] > example[1] &&
+					example[0] + example[1] > example[2] )
+					validTriangles++;
+			}
+
+			return validTriangles;
+		}
+	}
+}
