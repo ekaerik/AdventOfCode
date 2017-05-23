98c62f85ca13b8bdf4e3d45cbd264c65b1bfa09b
Unknown <eka.sundberg@gmail.com>
2017-05-23 16:51:10 +0200

    Uncompilable Day4

---
+++ AdventOfCOde/Day4.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCOde
+{
+	public class Day4
+	{
+		private Regex regex = new Regex(@"(.*?)(\d*?)\[(.*?)\]");
+		
+		public int RealRooms(params string[] rooms)
+		{
+			var result = 0;
+			foreach (var room in rooms)
+			{
+				var matches = regex.Match(room);
+				var encryption = matches.Groups[1].Value;
+				var sectorId = int.Parse(matches.Groups[2].Value);
+				var checksum = matches.Groups[3].Value;
+
+				var groups = (from l in encryption.ToCharArray()
+						   group l by l into g
+						   where g.Key != '-'
+						   orderby g.Count() descending
+						   select g).ToArray();
+				var ties = (from g in groups
+							group g by g.Count() into t
+							select t)
+							.ToArray();
+
+				var valid = false;
+				for (var i = 0; i < 5; i++)
+				{
+					var next = groups.SafeNext(i);
+					var tie = groups[i].Count() == next?.Count();
+					if (tie)
+					{
+						var nextLetter = ties[i].Key + 1;
+						if (checksum.SafeNext(i) != 
+					}
+					if (checksum[i] != groups[i].Key)
+					{
+						break;
+					}					
+					valid = true;
+				}
+				for (var i = 0; i < 5; i++)
+				{
+					
+				}
+
+				result = valid ? result + sectorId : result;
+			}
+			return result;
+		}
+	}
+
+	public static class ArrayExtensions
+	{
+		public static T SafeNext<T>(this IEnumerable<T> source, int index)
+		{
+			if (index + 1 <= source.Count())
+				return source.ElementAt(index + 1);
+			return default(T);
+		}
+
+	}
+}
---
+++ AdventOfCodeTest/Day4Test.cs
@@ -0,0 +1,58 @@
+﻿using AdventOfCOde;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AdventOfCodeTest
+{
+	[TestFixture]
+	public class Day4Test
+	{
+		[Test]
+		public void Should1()
+		{
+			var result = new Day4().RealRooms("aaaaa-bbb-z-y-x-123[abxyz]");
+
+			result.Should().Be(123);
+		}
+
+		[Test]
+		public void Should2()
+		{
+			var result = new Day4().RealRooms("a-b-c-d-e-f-g-h-987[abcde]");
+
+			result.Should().Be(987);
+		}
+
+		[Test]
+		public void Should3()
+		{
+			var result = new Day4().RealRooms("not-a-real-room-404[oarel]");
+
+			result.Should().Be(404);
+		}
+
+		[Test]
+		public void Should4()
+		{
+			var result = new Day4().RealRooms("totally-real-room-200[decoy]");
+
+			result.Should().Be(0);
+		}
+		
+		[Test]
+		public void ShouldCheckAlphabeticalInTie()
+		{
+			var result = new Day4().RealRooms("b-a-c-e-d-f-g-h-987[abcde]");
+
+			result.Should().Be(0);
+		}
+
+		[Test]
+		public void ShouldCheckCountAndTie()
+		{
+			var result = new Day4().RealRooms("aaa-z-b-111[abc]");
+
+			result.Should().Be(0);
+		}
+	}
+}
