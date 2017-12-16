0e42877561d0e0be4d1c4d038b22f93b566316b7
Unknown <eka.sundberg@gmail.com>
2017-12-16 09:27:23 +0100

    Day1

---
+++ 2017/Day1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using MoreLinq;
+
+namespace _2017
+{
+    public class Day1
+    {
+        public int Calculate(string input)
+        {
+            var sum = 0;
+            for (var i = 0; i < input.Length; i++)
+            {
+                var current = input[i];
+                var next = i == input.Length - 1 ? input[0] : input[i + 1];
+                if (current == next)
+                    sum += Int32.Parse(current.ToString());
+            }
+            return sum;
+        }
+    }
+}
