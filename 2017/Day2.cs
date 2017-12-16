377b42d63545b2cd5b79101be8a36966a4558885
Unknown <eka.sundberg@gmail.com>
2017-12-16 09:43:12 +0100

    Day2

---
+++ 2017/Day2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace _2017
+{
+    public class Day2
+    {
+        public int Calcualate(IEnumerable<int[]> input)
+        {
+            var checksum = 0;
+            foreach (var row in input)
+            {
+                var ordered = row.OrderBy(i => i).ToArray();
+                var largestDifferenceForRow = ordered.Last() - ordered.First();
+                checksum += largestDifferenceForRow;
+            }
+            
+            return checksum;
+        }
+        
+    }
+}
