829bdf15157afd2c562fabefb748f5c8417307ca
EkaSundberg <eka.sundberg@gmail.com>
2021-12-19 20:21:35 +0100

    Skip 5

---
+++ 2021/Day5.cs
@@ -0,0 +1,14 @@
+﻿namespace _2021
+{
+    public class Day5
+    {
+        public int Execute(((int x, int y) start, (int x, int y) end)[] input)
+        {
+            var straightLines = input
+                .Where(c => c.start.x == c.end.x || c.start.y == c.end.y)
+                .ToArray();
+
+            return 0;
+        }
+    }
+}
---
+++ 2021Test/Day5Test.cs
@@ -0,0 +1,40 @@
+﻿using _2021;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace _2021Test
+{
+    public class Day5Test : AdventOfCodeTest
+    {
+        public Day5Test(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        public class Part1
+        {
+            [Fact(Skip = "Not today")]
+            public void Should()
+            {
+                var example = new[]
+                {
+                    ((0,9), (5,9)),
+                    ((8,0), (0,8)),
+                    ((9,4), (3,4)),
+                    ((2,2), (2,1)),
+                    ((7,0), (7,4)),
+                    ((6,4), (2,0)),
+                    ((0,9), (2,9)),
+                    ((3,4), (1,4)),
+                    ((0,0), (8,8)),
+                    ((5,5), (8,2))
+                };
+                var target = new Day5();
+
+                var result = target.Execute(example);
+
+                result.Should().Be(5);
+            }
+        }
+    }
+}
